85f00a080526b96463e4553ffe52f0e33ad8db85
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-04-15 15:57:51 -0700

    successful series of diceRoll tests

--- _tests_/rpg.test.js
+++ _tests_/rpg.test.js
@@ -5,9 +5,14 @@ import { diceRoll } from './../src/rpg';
 import { winCondition } from './../src/rpg';
 
 test('Generates random number between 1-6', () => {
-  
-  expect(diceRoll).toBeGreaterThan(0);
-  expect(diceRoll).toBeLessThan(7);
+  expect(diceRoll()).toBeGreaterThan(0);
+  expect(diceRoll()).toBeLessThan(7);
+});
+
+test('Generates random number between 1-6', () => {
+  let result = Math.floor(Math.random() *6) + 1;
+  expect(result).toBeGreaterThan(0);
+  expect(result).toBeLessThan(7);
 });
 
 // test(''), () => {
