fd2a51fe2554329e58293dbde846edd8cdb0032a
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-04-16 09:33:13 -0700

    monsterClass chooser, battle property function of Game class

--- src/rpg.js
+++ src/rpg.js
@@ -2,11 +2,22 @@ export class Character {
   constructor (name, strength, health) {
     this.name = userName;
     this.strength = strength;
+    this.intellect = intellect;
+    this.dex = dex;
     this.health = health;
+    this.items = [];
+    this.maxItems = 5;
+    this.level = 1;
     
-    const warrior = new Character ("", 4, 10);
-    const wizard = new Character ("", 2, 10);
-    const rogue = new Character ("", 3, 10);
+    const warrior = new Character ("", 4, 2, 3, 10);
+    const wizard = new Character ("", 2, 4, 3, 10);
+    const rogue = new Character ("", 3, 3, 4, 10);
+  }
+};
+
+export class Monster extends Character {
+  monsterClassGenerator = function () {
+    return Math.floor(Math.random() * 3) +1;
   }
 };
 
@@ -22,9 +33,13 @@ export class Game {
   battleRoyal = function() {
     this.player = player;
     this.opponent = opponent;
-    if (player diceRoll)
+    if (player.diceRoll > opponent.diceRoll)
+      return opponent.health -= 1;
+      else 
+      return player.health -= 1;
   };
-
+  
+  
 
   // Check for win condition
   winCondition = function(player, opponent) {
